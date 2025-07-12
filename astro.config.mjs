2b6b42315129381c4d10e29ea1c34a82a0ad6c6d
robertbrice <rob.a.brice@gmail.com>
2025-07-12 11:52:30 +0100

    config file updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 
-  site: 'https://www.my-site.dev',
-  output: 'static',
-  build: {
-  inlineStylesheets: 'never',
-  },
-
-
-
-
+  output: 'static'
+ 
 });
