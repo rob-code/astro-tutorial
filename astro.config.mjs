6963a83d3743395b9fd6f49a9b251705443054a6
robertbrice <rob.a.brice@gmail.com>
2025-07-12 11:41:17 +0100

    more tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,15 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+
+  site: 'https://www.my-site.dev',
+  output: 'static',
+  build: {
+  inlineStylesheets: 'never',
+  },
+
+
+
+
+});
